735432dbccb1e375d04553923ee7b00ab048f6d1
Jonathan Florido-Carait <jonathan.carait@outlook.com>
2022-09-06 10:06:59 +1000

    after linting is configured

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "next/core-web-vitals",
+    "plugin:jest/recommended",
+    "plugin:jest/style",
+    "plugin:testing-library/react",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "tsconfig.json",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {},
+};
